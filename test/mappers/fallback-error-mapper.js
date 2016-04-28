02be47b00115472aa3de23f449b1c3e3724cb228
Rui Marinho <rui@uphold.com>
2016-04-28 02:27:20 +0100

    Add linters

--- test/mappers/fallback-error-mapper.js
+++ test/mappers/fallback-error-mapper.js
@@ -4,15 +4,14 @@
  * Module dependencies.
  */
 
-let mapper = require('../../mappers/fallback-error-mapper');
-let should = require('should');
+const mapper = require('../../mappers/fallback-error-mapper');
 
 /**
  * Test `FallbackErrorMapper`.
  */
 
-describe('FallbackErrorMapper', function() {
-  it('should always return internal server error', function() {
-    mapper.map().should.eql({ status: 500, body: { code: 'internal_server_error', message: 'Internal Server Error' }});
+describe('FallbackErrorMapper', () => {
+  it('should always return internal server error', () => {
+    mapper.map().should.eql({ body: { code: 'internal_server_error', message: 'Internal Server Error' }, status: 500 });
   });
 });
