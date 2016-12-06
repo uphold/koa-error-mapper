e50d0b3a146e09809dc0020749eafec14fc3bb3f
Ricardo Gama <rgama@uphold.com>
2016-12-06 22:14:40 +0000

    Update HttpErrorMapper to support http-standard-error

--- test/mappers/http-error-mapper_test.js
+++ test/mappers/http-error-mapper_test.js
@@ -4,8 +4,8 @@
  * Module dependencies.
  */
 
-const HttpError = require('http-errors');
-const mapper = require('../../mappers/http-error-mapper');
+const HttpError = require('standard-http-error');
+const httpErrorMapper = require('../../mappers/http-error-mapper');
 const should = require('should');
 
 /**
@@ -13,24 +13,43 @@ const should = require('should');
  */
 
 describe('HttpErrorMapper', () => {
-  it('should not map any error that is not an instance of `HttpError`', () => {
-    should.not.exist(mapper.map({}));
+  describe('when `HttpError` is not provided', () => {
+    const mapper = httpErrorMapper();
+
+    it('should not map any error that is not an instance of `HttpError`', () => {
+      should.not.exist(mapper.map({}));
+    });
+
+    it('should return a regular http message if `HttpError` only has a status code set', () => {
+      mapper.map(new HttpError(401)).should.eql({ body: { code: 'unauthorized', message: 'Unauthorized' }, headers: undefined, status: 401 });
+    });
+
+    it('should return `HttpError` message and its code in snake case format', () => {
+      mapper.map(new HttpError(401, 'Foo Bar')).should.eql({ body: { code: 'foo_bar', message: 'Foo Bar' }, headers: undefined, status: 401 });
+    });
+
+    it('should return `HttpError` headers', () => {
+      mapper.map(new HttpError(401, 'foo', { headers: { biz: 'baz' } })).should.eql({ body: { code: 'foo', message: 'foo' }, headers: { biz: 'baz' }, status: 401 });
+    });
   });
 
-  it('should not expose the message error if `HttpError` does not have a status code set', () => {
-    mapper.map(new HttpError('foo')).should.eql({ body: { code: 'internal_server_error', message: 'Internal Server Error' }, status: 500 });
-  });
+  describe('when `HttpError` is provided', () => {
+    const mapper = httpErrorMapper(HttpError);
 
-  it('should not expose the message error if `HttpError` does not have an exposable status code', () => {
-    mapper.map(new HttpError(503, 'Foo')).should.eql({ body: { code: 'service_unavailable', message: 'Service Unavailable' }, status: 503 });
-  });
+    it('should not map any error that is not an instance of `HttpError`', () => {
+      should.not.exist(mapper.map({}));
+    });
 
-  it('should return a regular http message if `HttpError` only has a status code set', () => {
-    mapper.map(new HttpError(401)).should.eql({ body: { code: 'unauthorized', message: 'Unauthorized' }, status: 401 });
-  });
+    it('should return a regular http message if `HttpError` only has a status code set', () => {
+      mapper.map(new HttpError(401)).should.eql({ body: { code: 'unauthorized', message: 'Unauthorized' }, headers: undefined, status: 401 });
+    });
 
-  it('should return the error message if `HttpError` has an exposable status code and a message set', () => {
-    mapper.map(new HttpError(401, 'Foo')).should.eql({ body: { code: 'unauthorized', message: 'Foo' }, status: 401 });
-    mapper.map(new HttpError('Foo', 401)).should.eql({ body: { code: 'unauthorized', message: 'Foo' }, status: 401 });
+    it('should return `HttpError` message and its code in snake case format', () => {
+      mapper.map(new HttpError(401, 'Foo Bar')).should.eql({ body: { code: 'foo_bar', message: 'Foo Bar' }, headers: undefined, status: 401 });
+    });
+
+    it('should return `HttpError` headers', () => {
+      mapper.map(new HttpError(401, 'foo', { headers: { biz: 'baz' } })).should.eql({ body: { code: 'foo', message: 'foo' }, headers: { biz: 'baz' }, status: 401 });
+    });
   });
 });
